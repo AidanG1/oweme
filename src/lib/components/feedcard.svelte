d92fa76265d3abf603afb3e1a613f4a192d61cdf
Flipfloppm <franklin.s.dai@gmail.com>
2024-09-21 13:46:09 -0500

    static homepage finished

--- src/lib/components/feedcard.svelte
+++ src/lib/components/feedcard.svelte
@@ -10,18 +10,35 @@
 		incoming: boolean
 		resolved: boolean
 	}
-	// let { name, description, price }: MyProps = $props()
+	let { name, description, time, price, incoming, resolved }: MyProps = $props()
+	let divClass: string
+	if (resolved) {
+		divClass = 'flex flex-row opacity-50'
+	} else {
+		divClass = 'flex flex-row'
+	}
+	let date = time.toLocaleString('en-US', {
+		day: 'numeric',
+		month: 'short'
+	})
 </script>
 
-<div class="flex flex-row opacity-50">
+<div class={divClass}>
 	<Avatar.Root>
 		<Avatar.Image src="https://github.com/shadcn.png" alt="shadcn img" />
 		<Avatar.Fallback>CN</Avatar.Fallback>
 	</Avatar.Root>
-	<div class="mx-2">
-		<p class="text-s">Franklin</p>
-		<p class="text-xs opacity-60">Pizza</p>
+	<div class="mx-2 flex-grow">
+		<div class="flex flex-row items-center gap-2">
+			<p class="text-s">{name}</p>
+			<p class="text-xs opacity-60">{date}</p>
+		</div>
+		<p class="text-xs opacity-60">{description}</p>
 	</div>
-	<p class="ml-auto">+ 50.0</p>
+	{#if incoming}
+		<p class="ml-auto text-green-500">+ {price}</p>
+	{:else}
+		<p class="ml-auto text-red-500">- {price}</p>
+	{/if}
 </div>
 <Separator class="my-4" />
