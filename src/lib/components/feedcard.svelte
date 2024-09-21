62533d4dd4a1396de65629d416073f5f90b45b36
Flipfloppm <franklin.s.dai@gmail.com>
2024-09-21 11:16:14 -0500

    wip

---
+++ src/lib/components/feedcard.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    import * as Avatar from "$lib/components/ui/avatar";
+    import { Separator } from "$lib/components/ui/separator";
+</script>
+
+<div class="flex flex-row">
+    <Avatar.Root>
+        <Avatar.Image src="https://github.com/shadcn.png" alt="shadcn img" />
+        <Avatar.Fallback>CN</Avatar.Fallback>
+    </Avatar.Root>
+    <div class="mx-2">
+        <p class="text-s">Franklin</p>
+        <p class="text-xs opacity-60">Pizza</p>
+    </div>
+    <p class="ml-auto">+ $50.0</p>
+</div>
+<Separator class="my-4"/>
