4bfd0538381d5bbfbb6e8fe6b0e4fcb4b2e55b97
Aidan <agentaidan@gmail.com>
2024-09-21 16:35:19 -0500

    start work on friends

---
+++ src/routes/friends/+layout.ts
@@ -0,0 +1,24 @@
+import { logInCheck } from '$lib/auth.svelte'
+import { error as SKerror } from '@sveltejs/kit'
+import { supabase } from '$lib/db'
+import { sesh } from '$lib/auth.svelte'
+
+export const load = async () => {
+    await logInCheck()
+
+    const { data, error } = await supabase
+        .from('friends')
+        .select(`friend_1 (id, venmo, name, auth.users (email)),
+        friend_2 (id, venmo, name, auth.users (email))
+        `)
+        .or(`friend_1.eq.${sesh.session?.user.id},friend_2.eq.${sesh.session?.user.id}`)
+        .eq('accepted', true)
+
+    if (error) {
+        SKerror(500, error.message)
+    }
+
+    return {
+        friends: data
+    }
+}
