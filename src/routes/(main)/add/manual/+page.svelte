04f3435571fbb28b49d16ef8bef065137fef740d
Flipfloppm <franklin.s.dai@gmail.com>
2024-09-22 03:19:08 -0500

    styled manual input page

--- src/routes/(main)/add/manual/+page.svelte
+++ src/routes/(main)/add/manual/+page.svelte
@@ -1,11 +1,12 @@
 <script lang="ts">
+	import { sesh } from '$lib/auth.svelte'
 	import { Button } from '$lib/components/ui/button'
 	import { Input } from '$lib/components/ui/input'
 	import { Label } from '$lib/components/ui/label'
-	import Plus from 'lucide-svelte/icons/plus'
 	import { supabase } from '$lib/db'
-	import { sesh } from '$lib/auth.svelte'
-
+	import Plus from 'lucide-svelte/icons/plus'
+	import X from 'lucide-svelte/icons/x'
+	import { slide } from 'svelte/transition'
 	interface Item {
 		name?: string
 		price?: number
@@ -65,45 +66,52 @@
 			return
 		}
 	}
+
+	function deleteItem(index: number) {
+		items = items.filter((_, i) => i !== index)
+	}
 </script>
 
-<div>
-	<form class="flex">
+<div class="m-5">
+	<form class="mb-5 flex">
 		<div class="flex w-full flex-col gap-1.5">
-			<Label for="transaction_name">Transaction name</Label>
-			<Input
-				id="transaction_name"
-				placeholder="Transaction name"
-				required
-				bind:value={transaction_name}
-			/>
+			<Label for="transaction_name">Transaction Name</Label>
+			<Input id="transaction_name" required bind:value={transaction_name} />
 		</div>
 	</form>
-	{#each items as item}
-		<form class="flex">
-			<div class="flex w-full max-w-sm flex-col gap-1.5">
-				<Label for="item_name">Item name</Label>
-				<Input id="item_name" placeholder="Item name" required bind:value={item.name} />
-			</div>
-			<div class="flex w-full max-w-sm flex-col gap-1.5">
-				<Label for="price">Price</Label>
-				<Input id="item_price" placeholder="Item price" required bind:value={item.price} />
-			</div>
-		</form>
+	{#each items as item, index (index)}
+		<div transition:slide={{ duration: 300 }} class="mb-4">
+			<form class="flex items-end">
+				<div class="mr-4 flex w-full max-w-sm flex-col gap-1.5">
+					<Label for="item_name_{index}">Item name</Label>
+					<Input id="item_name_{index}" required bind:value={item.name} />
+				</div>
+				<div class="flex w-full max-w-sm flex-col gap-1.5">
+					<div class="flex flex-row">
+						<Label for="price_{index}">Price</Label>
+						<Button
+							variant="destructive"
+							class="my-auto mb-0.5 ml-auto h-4 w-4 p-0"
+							on:click={() => deleteItem(index)}
+							aria-label="Delete item"
+						>
+							<X class="h-4 w-4" />
+						</Button>
+					</div>
+					<Input id="item_price_{index}" required bind:value={item.price} />
+				</div>
+			</form>
+		</div>
 	{/each}
-	<Button
-		class=""
-		on:click={() => {
-			items.push({} as Item)
-		}}
-	>
-		<Plus />
-	</Button>
-	<Button
-		class=""
-		type="submit"
-		on:click={submit_transaction}
-	>
-		Submit
-	</Button>
+	<div class="mt-5 flex flex-row justify-between">
+		<Button
+			class=""
+			on:click={() => {
+				items.push({} as Item)
+			}}
+		>
+			<Plus />
+		</Button>
+		<Button class="" type="submit" on:click={submit_transaction}>Submit Item List</Button>
+	</div>
 </div>
