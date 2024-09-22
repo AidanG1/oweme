753e031444ff26cc2be8e01335230c0bb882c1c5
Yimo Wang <yw104@rice.edu>
2024-09-22 07:38:24 -0500

    fix totals page

--- src/routes/(main)/transactions/[transaction]/total/+page.svelte
+++ src/routes/(main)/transactions/[transaction]/total/+page.svelte
@@ -4,11 +4,14 @@
 
 	let { data, transData } = $page.data.props
 
+	console.log('data here: ', data)
+
 	let food_map = new Map()
 	data.forEach((e) => {
 		e.item['amount_basis_points'] = e.amount_basis_points
 		if (food_map.has(e.email)) {
-			food_map.set(e.email, [...food_map.get(e.mail), e.item])
+			let prev = food_map.get(e.email)
+			food_map.set(e.email, [...prev, e.item])
 		} else {
 			food_map.set(e.email, [e.item])
 		}
@@ -18,15 +21,10 @@
 		if (total_map.has(e.email)) {
 			total_map.set(
 				e.email,
-				total_map.get(e.email) +
-					total_map.get(e.email) +
-					((e.item.amount_basis_points / 10000) * e.item.amount_cents) / 100
-			)
-		} else {
-			total_map.set(
-				e.email,
 				total_map.get(e.email) + ((e.item.amount_basis_points / 10000) * e.item.amount_cents) / 100
 			)
+		} else {
+			total_map.set(e.email, ((e.item.amount_basis_points / 10000) * e.item.amount_cents) / 100)
 		}
 	})
 </script>
@@ -37,7 +35,7 @@
 		<Card.Root>
 			<Card.Header class="flex flex-row justify-between">
 				<Card.Title class="my-auto">{email}</Card.Title>
-				<Card.Title class="my-auto">40.00</Card.Title>
+				<Card.Title class="my-auto">{Number(total_map.get(email)).toFixed(2)}</Card.Title>
 			</Card.Header>
 			<Card.Content class="">
 				{#each food_map.get(email) as item}
